de787b040e9f7dcc025d2f85cb1159b1ff4a98a5
dekim1028 <daeun235194@gmail.com>
2020-08-21 15:17:40 +0900

    useReducer 예제

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,22 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Info from './Info';
 
 
 function App() {
-  const [visible,setVisible] = useState(false);
 
   return (
-    <div>
-      <button
-        onClick={()=>{
-          setVisible(!visible);
-        }}
-      >
-        {visible?'숨기기':'보이기'}
-      </button>
-      <hr/>
-      {visible&&<Info/>}
-    </div>
+    <Info/>
     
   );
 }
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,56 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React,{useReducer} from 'react';
+
+function reducer(state, action){
+    return{
+        ...state,
+        [action.name]:action.value
+    };
+}
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-
-    /* 1. 리액트 컴포넌트가 렌더링 될때마다 수행 ( componentDidMount + componentDidUpdate )
-    useEffect(()=>{
-        console.log("렌더링이 완료되었습니다.");
-        console.log({
-            name,
-            nickname
-        })
+    const [state, dispatch] = useReducer(reducer,{
+        name:'',
+        nickname:''
     });
-    */
-
-    /* 2. 컴포넌트가 처음 렌더링 될때만 수행 ( componentDidMount )
-    useEffect(()=>{
-        console.log("마운트될 때만 실행됩니다.")
-    },[]);
-    */
-
-    /* 3. 특정 값이 변경 될때만 수행 ( componentDidUpdate )
-    useEffect(()=>{
-        console.log("업데이트될 때만 실행됩니다.")
-    },[name]);
-    */
-
-    /* 4. component가 언마운트 되기 전 or 업데이트 직전 수행시 */
-    useEffect(()=>{
-        console.log("effect");
-        console.log(name);
-
-        return()=>{
-            console.log('cleanup');
-            console.log(name);
-        }
-
-    },[name]);
-
-    const onChangeName = (e) =>{
-        setName(e.target.value);
-    }
-
-    const onChangeNickname = (e) =>{
-        setNickname(e.target.value);
-    }
+    const {name,nickname} = state;
+    
+    const onChange = e =>{
+        dispatch(e.target);
+    }   
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName}/>
-                <input value={nickname} onChange={onChangeNickname}/>
+                <input name="name" value={name} onChange={onChange}/>
+                <input name="nickname" value={nickname} onChange={onChange}/>
             </div>
             <div>
                 <div>
