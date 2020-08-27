2d7b4940800dfec71c0540732b11cd04bf1011d8
dekim1028 <daeun235194@gmail.com>
2020-08-27 16:32:20 +0900

    TodoTemplate 생성

--- todo-app-myself/src/App.js
+++ todo-app-myself/src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoTemplate from './component/TodoTemplate';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>리스트</TodoTemplate>
   );
-}
+};
 
 export default App;
