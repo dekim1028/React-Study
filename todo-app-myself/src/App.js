7b9e915b2ad40fc5d7d214c711afe6f75fbd3fdd
dekim1028 <daeun235194@gmail.com>
2020-08-29 18:16:13 +0900

    listitem 추가 기능 개발

--- todo-app-myself/src/App.js
+++ todo-app-myself/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:'컴포넌트 공부하기',
+      checked:false,
+    },
+    {
+      id:2,
+      text:'컴포넌트 공부하기2',
+      checked:false,
+    },
+    {
+      id:3,
+      text:'컴포넌트 공부하기3',
+      checked:true,
+    },
+  ]);
+
+  const nextId = useRef('4');
+
+  const onInsert = useCallback(
+    text=>{
+      const todo ={
+        id:nextId.current,
+        text,
+        checked:false
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
+
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList/>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 };
--- todo-app-myself/src/component/TodoInsert.js
+++ todo-app-myself/src/component/TodoInsert.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{useState, useCallback} from 'react';
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e=>{
+        setValue(e.target.value);
+    },[]);
+
+    const onSubmit = useCallback(
+        e=>{
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        },
+        [onInsert,value]
+    )
+
     return (
-        <div className="TodoInput">
-            <input className="inputBox" type="text" placeholder="할 일을 입력해주세요."/>
+        <form className="TodoInput" onSubmit={onSubmit}>
+            <input className="inputBox" type="text" value={value} onChange={onChange} placeholder="할 일을 입력해주세요."/>
             <button className="addBtn" type="submit"><MdAdd/></button>
-        </div>
+        </form>
     );
 };
 
