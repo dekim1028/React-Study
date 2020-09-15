576ecfbeacbcf12140031bc90d22968a4f04a718
dekim1028 <daeun235194@gmail.com>
2020-09-15 13:42:02 +0900

    render 함수 생성

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,3 +1,5 @@
+import {createStore} from 'redux';
+
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
@@ -35,3 +37,20 @@ function reducer(state=initialState, action){
             };
     }
 }
+
+const store = createStore(reducer);
+
+const render = () =>{
+    const state = store.getState(); //현재 상태를 불러온다.
+    //토글 처리
+    if(state.toggle){
+        divToggle.classList.add('active');
+    }else{
+        divToggle.classList.remove('active');
+    }
+
+    //카운터 처리
+    counter.innerText = state.counter;
+}
+
+render();
