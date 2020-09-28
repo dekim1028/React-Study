696ce886b5ba90b2d3d87e8f708873e575b0e44f
dekim1028 <daeun235194@gmail.com>
2020-09-28 15:21:54 +0900

    서버에서 Redux 설정 및 PreloadContext 사용하기

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -4,8 +4,12 @@ import express from 'express';
 import {StaticRouter} from 'react-router-dom';
 import App from './App';
 import path from 'path';
-
 import fs from 'fs';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
 
 // asset-manifest.json에서 파일 경로들을 조회
 const manifest = JSON.parse(
@@ -17,7 +21,7 @@ const chunks = Object.keys(manifest.files)
     .map(key=>`<script src="${manifest.files[key]}"></script>`) //스크립트 태그로 변환
     .join(' '); //합침
 
-function createPage(root){
+function createPage(root, stateScript){
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -33,6 +37,7 @@ function createPage(root){
         <div id="root">
             ${root}
         </div>
+        ${stateScript}
         <script src="${manifest.files['runtime~main.js']}"></script>
         ${chunks}
         <script src="${manifest.files['main.js']}"></script>
@@ -44,17 +49,38 @@ function createPage(root){
 const app = express();
 
 //서버사이드 렌더링을 처리할 핸들러 함수
-const serverRender = (req,res,next)=>{
+const serverRender = async(req,res,next)=>{
     //이 함수는 404가 떠야하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해준다.
 
     const context = {};
+    const store = createStore(rootReducer,applyMiddleware(thunk));
+
+    const preloadContext={
+        done:false,
+        promises:[]
+    };
+
     const jsx = (
-        <StaticRouter location={req.url} context={context}>
-            <App/>
-        </StaticRouter>
+        <PreloadContext.Provider value={preloadContext}>
+            <Provider store={store}>
+                <StaticRouter location={req.url} context={context}>
+                    <App/>
+                </StaticRouter>
+            </Provider>
+        </PreloadContext.Provider>
     );
+
+    ReactDomServer.renderToStaticMarkup(jsx);
+    try{
+        await Promise.all(preloadContext.promises);
+    }catch(e){
+        return res.status(500);
+    }
+    preloadContext.done=true;
     const root = ReactDomServer.renderToString(jsx);
-    res.send(createPage(root));
+    const stateString = JSON.stringify(store.getState()).replace(/</g,'\\u003c');
+    const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`;
+    res.send(createPage(root,stateScript));
 }
 
 const serve = express.static(path.resolve('./build'),{
