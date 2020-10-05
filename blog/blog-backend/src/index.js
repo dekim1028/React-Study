61722fae8e181915d1832b7d0dbc71e275c50ca8
dekim1028 <daeun235194@gmail.com>
2020-10-05 13:14:06 +0900

    async/await 사용

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use((ctx,next)=>{
+app.use(async (ctx,next)=>{
     console.log(ctx.url);
     console.log(1);
     if(ctx.query.authorized !== '1'){
@@ -10,10 +10,8 @@ app.use((ctx,next)=>{
         return;
     }
 
-    //next 함수는 Promise를 반환
-    next().then(()=>{
-        console.log('END');
-    });
+    await next();
+    console.log('END');
 });
 
 app.use((ctx,next)=>{
