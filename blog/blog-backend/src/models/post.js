7a9b458eaac274a9ee14a7826ec3b64ef373faa7
dekim1028 <daeun235194@gmail.com>
2020-10-09 13:31:06 +0900

    포스트 작성시 사용자 정보 넣기

--- blog/blog-backend/src/models/post.js
+++ blog/blog-backend/src/models/post.js
@@ -1,6 +1,4 @@
-import mongoose from 'mongoose';
-
-const {Schema} = mongoose;
+import mongoose,{Schema} from 'mongoose';
 
 const PostSchema = new Schema({
     title:String,
@@ -9,7 +7,11 @@ const PostSchema = new Schema({
     publishedData:{
         type:Date,
         default:Date.now, //현재 날짜를 기본값으로 지정
-    }
+    },
+    user:{
+        _id:mongoose.Types.ObjectId,
+        username:String,
+    },
 });
 
 const Post = mongoose.model('Post',PostSchema);
