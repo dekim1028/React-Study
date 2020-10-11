22ada66b9f23ca8b4ce446990227bffec7233aba
dekim1028 <daeun235194@gmail.com>
2020-10-11 14:14:35 +0900

    회원가입 구현

--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {changeField, initializeForm} from '../../modules/auth';
+import {changeField, initializeForm, register} from '../../modules/auth';
 import { useEffect } from 'react';
 import AuthForm from '../../components/auth/AuthForm';
+import {check} from '../../modules/user';
+import {useHistory,withRouter} from 'react-router-dom';
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth})=>({
-        form:auth.register
+    const history = useHistory();
+
+    const {form, auth, authError, user} = useSelector(({auth, user})=>({
+        form:auth.register,
+        auth:auth.auth,
+        authError:auth.authError,
+        user:user.user
     }));
     //인폿 변경 이벤트 핸들러
     const onChange = e =>{
@@ -24,12 +31,38 @@ const RegisterForm = () => {
     //폼 등록 이벤트 핸들러
     const onSubmit = e =>{
         e.preventDefault();
+        const {username, password, passwordConfirm} = form;
+        if(password!==passwordConfirm){
+            //TODO: 오류처리
+            return;
+        }
+        dispatch(register({username,password}));
     };
 
     //컴포넌트가 처음 렌더링될 때 form을 초기화함
     useEffect(()=>{
         dispatch(initializeForm('register'));
     },[dispatch]);
+
+    useEffect(()=>{
+        if(authError){
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if(auth){
+            console.log('회원가입 성공');
+            console.log(auth);
+            dispatch(check());
+        }
+    },[auth,authError,dispatch]);
+
+    //user 값이 잘 설정되었는지 확인
+    useEffect(()=>{
+        if(user){
+            history.push('/');  //홈 화면으로 이동
+        }
+    },[history,user]);
 
     return (
         <AuthForm
@@ -41,4 +74,4 @@ const RegisterForm = () => {
     );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
