b0cddb8f83f750f734f7bbd18267b61970691544
dekim1028 <daeun235194@gmail.com>
2020-10-11 14:22:48 +0900

    로그인 구현

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {changeField, initializeForm} from '../../modules/auth';
-import { useEffect } from 'react';
+import {withRouter} from 'react-router-dom';
+import {changeField, initializeForm, login} from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import {check} from '../../modules/user';
 
-const LoginForm = () => {
+const LoginForm = ({history}) => {
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth})=>({
-        form:auth.login
+    const {form, auth, authError, user} = useSelector(({auth, user})=>({
+        form:auth.login,
+        auth:auth.auth,
+        authError:auth.authError,
+        user:user.user
     }));
     //인폿 변경 이벤트 핸들러
     const onChange = e =>{
@@ -24,12 +28,32 @@ const LoginForm = () => {
     //폼 등록 이벤트 핸들러
     const onSubmit = e =>{
         e.preventDefault();
+        const {username,password} = form;
+        dispatch(login({username,password}));
     };
 
     //컴포넌트가 처음 렌더링될 때 form을 초기화함
     useEffect(()=>{
         dispatch(initializeForm('login'));
     },[dispatch]);
+
+    useEffect(()=>{
+        if(authError){
+            console.log("오류발생");
+            console.log(authError);
+            return;
+        }
+        if(auth){
+            console.log("로그인 성공");
+            dispatch(check());
+        }
+    },[auth, authError, dispatch]);
+
+    useEffect(()=>{
+        if(user){
+            history.push("/");
+        }
+    },[history,user]);
 
     return (
         <AuthForm
@@ -41,4 +65,4 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
