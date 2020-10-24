6938e6065020b6d01a5a292d33088722c2fbb76b
dekim1028 <daeun235194@gmail.com>
2020-10-24 15:30:08 +0900

    페이지네이션 구현

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostsListContainer from '../containers/posts/PostsListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 
 const PostListPage = () => {
     return (
         <div>
             <HeaderContainer/>
             <PostsListContainer/>
+            <PaginationContainer/>
         </div>
     );
 };
